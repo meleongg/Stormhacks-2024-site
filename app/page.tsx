4be97f16c79dd67aa716b660196b95e08d7e76d4
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2024-05-18 14:12:59 -0700

    MUI setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Button from "@mui/material/Button";
+
 export default function Home() {
   return (
     <main>
       <div>Hello world</div>
+      <Button variant="contained">Hello world</Button>
     </main>
   );
 }
