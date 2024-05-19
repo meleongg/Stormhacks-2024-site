1cbc4bddee3a08b91661e7b9315a878ab09107e4
Melvin Teo <62491197+meleongg@users.noreply.github.com>
2024-05-18 18:21:42 -0700

    Add more content onto site

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,13 @@ import AppBar from "@mui/material/AppBar";
 import Button from "@mui/material/Button";
 import Toolbar from "@mui/material/Toolbar";
 
+import Box from "@mui/material/Box"; // Import the Box component from Material-UI
+
 import Image from "next/image"; // Import next/image for optimized image rendering
 
 // Import images from the local folder
 import rabbit from "../public/images/Energizer Rabbit.jpeg";
+import logo from "../public/images/logo.svg";
 
 export default function Home() {
   const handleScroll = (sectionId: string) => {
@@ -23,22 +26,65 @@ export default function Home() {
   };
 
   return (
-    <main>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Logo
-          </Typography>
+    <main style={{ backgroundColor: "#001143", fontSize: "18px" }}>
+      <AppBar position="static" sx={{ color: "#333" }}>
+        <Toolbar
+          sx={{
+            backgroundColor: "#B2B1DE",
+            color: "#333",
+          }}
+        >
+          {/* Use flexbox to split content into two sections */}
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            width="100%"
+          >
+            {/* Left section */}
+            <Box display="flex" alignItems="center" gap={1}>
+              {/* Logo */}
+              <Image src={logo} alt="Logo" width={50} height={50} />
+              <Typography variant="h6">HackerHare</Typography>
+            </Box>
 
-          <Button color="inherit" onClick={() => handleScroll("home")}>
-            Home
-          </Button>
-          <Button color="inherit" onClick={() => handleScroll("features")}>
-            Features
-          </Button>
-          <Button color="inherit" onClick={() => handleScroll("contact")}>
-            Contact
-          </Button>
+            {/* Right section */}
+            <Box>
+              {/* Buttons */}
+              <Button
+                color="inherit"
+                onClick={() => handleScroll("home")}
+                sx={{ textTransform: "none" }}
+              >
+                Home
+              </Button>
+              <Button
+                color="inherit"
+                onClick={() => handleScroll("features")}
+                sx={{ textTransform: "none" }}
+              >
+                Features
+              </Button>
+              <Button
+                color="inherit"
+                onClick={() => handleScroll("cta")}
+                sx={{
+                  backgroundColor: "#F0B256",
+                  color: "#333",
+                  textTransform: "none", // Disable all-uppercase transformation
+                  "&:hover": {
+                    backgroundColor: "#FFD9A6", // Complementary color for hover
+                    // Additional hover styles if needed
+                  },
+                  marginLeft: "10px",
+                  paddingLeft: "20px",
+                  paddingRight: "20px",
+                }}
+              >
+                Add to Chrome
+              </Button>
+            </Box>
+          </Box>
         </Toolbar>
       </AppBar>
       <Container id="home" style={{ padding: "100px 0" }}>
@@ -55,17 +101,49 @@ export default function Home() {
           This is the Features section of the landing page.
         </Typography>
       </Container>
-      <Container id="contact" style={{ padding: "100px 0" }}>
-        <Typography variant="h2" gutterBottom>
-          Contact Section
-        </Typography>
-        <Typography variant="body1">
-          This is the Contact section of the landing page.
-        </Typography>
+      <Container id="cta" style={{ padding: "100px 0" }}>
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Typography
+            variant="body1"
+            textAlign="center"
+            marginBottom={2}
+            sx={{ width: "300px", fontSize: "22px" }}
+          >
+            Ready to safeguard your online form-filling experience?
+          </Typography>
+          <Button
+            sx={{
+              backgroundColor: "#F0B256",
+              color: "#333",
+              textTransform: "none", // Disable all-uppercase transformation
+              "&:hover": {
+                backgroundColor: "#FFD9A6", // Complementary color for hover
+                // Additional hover styles if needed
+              },
+            }}
+          >
+            Use HackerHare Now
+          </Button>
+        </Box>
       </Container>
       <AppBar position="static" color="primary">
-        <Toolbar>
-          <Typography variant="body1" color="inherit" sx={{ flexGrow: 1 }}>
+        <Toolbar
+          sx={{
+            backgroundColor: "#B2B1DE",
+            textAlign: "center",
+            color: "#333",
+          }}
+        >
+          <Typography
+            variant="body1"
+            color="inherit"
+            sx={{ flexGrow: 1, backgroundColor: "#B2B1DE" }}
+          >
             © {new Date().getFullYear()} Your Company. All rights reserved.
           </Typography>
         </Toolbar>
